75bcfba570990ab66fa4e6ad4f2cb3cb9f8ac19e
山本憲吾 <yamamotokengo@yamamotokengonoMacBook-Pro.local>
2020-11-22 14:36:49 +0900

    JSとCSSリンター設定

---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  rules: {
+    indentation: 2,
+    'string-quotes': 'double',
+    'declaration-block-single-line-max-declarations': 1,
+    'max-empty-lines': 2,
+    'selector-max-specificity': '2,2,0',
+  },
+};
