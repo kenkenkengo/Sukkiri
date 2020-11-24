f947b02ba4186d0784a3e52c2e4f2bd7f4ab4d33
山本憲吾 <yamamotokengo@yamamotokengonoMacBook-Pro.local>
2020-11-24 22:34:00 +0900

    Userモデルバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  before_save :downcase_email
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
 
   validates :username, presence: true, length: { maximum: 20 }
-  validates :name, presence: true, length: { maximum: 20 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  private
+    def downcase_email
+      self.email = email.downcase
+    end
 end
