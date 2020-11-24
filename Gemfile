7f0f77e5b13fc58f0d3fa50b39b5d051a25294da
山本憲吾 <yamamotokengo@yamamotokengonoMacBook-Pro.local>
2020-11-24 22:36:28 +0900

    Userモデルスペック作成

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'devise'
 gem 'devise-i18n'
 gem 'devise-i18n-views'
+gem 'faker'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,2 +1,57 @@
 RSpec.describe User, type: :model do
+  let(:user) { create(:user) }
+
+  context "バリデーション" do
+    it "ユーザーネーム、メールアドレスが存在すれば有効である" do
+      expect(user).to be_valid
+    end
+
+    it "ユーザーネームがなければ無効な状態である" do
+      user = build(:user, username: nil)
+      user.valid?
+      expect(user.errors[:username]).to include("を入力してください")
+    end
+
+    it "ユーザーネームが20文字以内である" do
+      user = build(:user, username: "a" * 21)
+      user.valid?
+      expect(user.errors[:username]).to include("は20文字以内で入力してください")
+    end
+
+    it "メールアドレスがなければ無効な状態である" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
+
+    it "メールアドレスが255文字以内であること" do
+      user = build(:user, email: "#{"a" * 244}@example.com")
+      user.valid?
+      expect(user.errors[:email]).to include("は255文字以内で入力してください")
+    end
+
+    it "重複したメールアドレスなら無効な状態であること" do
+      other_user = build(:user, email: user.email)
+      other_user.valid?
+      expect(other_user.errors[:email]).to include("はすでに存在します")
+    end
+
+    it "メールアドレスは小文字で保存されること" do
+      email = "ExamPle@example.com"
+      user = create(:user, email: email)
+      expect(user.email).to eq email.downcase
+    end
+
+    it "パスワードがなければ無効な状態であること" do
+      user = build(:user, password: nil, password_confirmation: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+    end
+
+    it "パスワードが6文字以上であること" do
+      user = build(:user, password: "a" * 6, password_confirmation: "a" * 6)
+      user.valid?
+      expect(user).to be_valid
+    end
+  end
 end
