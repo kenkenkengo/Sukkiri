9c1d0ed0895d41cf329da27bc688056bd7b3d1b2
山本憲吾 <yamamotokengo@yamamotokengonoMacBook-Pro.local>
2020-11-25 16:04:17 +0900

    Userモデルバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,11 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true,
+                       length: { minimum: 6 },
+                       confirmation: true,
+                       on: :create
+  validates :password_confirmation, presence: true
 
   private
 
