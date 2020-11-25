a7c5f3ee5b6475b9fd95fba37c826d4ad2ae7c22
山本憲吾 <yamamotokengo@yamamotokengonoMacBook-Pro.local>
2020-11-26 00:13:32 +0900

    ユーザープロフィール画像表示ヘルパー追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,11 @@ module ApplicationHelper
       "#{page_title} | #{BASE_TITLE}"
     end
   end
+
+  def gravatar_for(user, size: 80)
+    return user.profile_photo unless user.profile_photo.nil?
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
