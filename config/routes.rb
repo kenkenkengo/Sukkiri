304b19318d89b9af782763877efd2bc458675124
山本憲吾 <yamamotokengo@yamamotokengonoMacBook-Pro.local>
2020-11-26 00:08:08 +0900

    ユーザー詳細ページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: { registrations: 'registrations' }
+  devise_scope :user do
+    get '/users/password', to: 'registrations#edit_password'
+    patch '/users/password/edit', to: 'registrations#update_password'
+  end
+
   root 'static_pages#home'
   get :about, to: 'static_pages#about'
   get :terms, to: 'static_pages#terms'
+  resources :users, only: [:show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_011554) do
+ActiveRecord::Schema.define(version: 2020_11_25_074451) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_11_24_011554) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
