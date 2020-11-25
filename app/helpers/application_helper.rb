f5335992879683bbfc78903741862a5a97d95b64
山本憲吾 <yamamotokengo@yamamotokengonoMacBook-Pro.local>
2020-11-26 03:04:52 +0900

    Rubocopによる修正と余分コード削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
 
   def gravatar_for(user, size: 80)
     return user.profile_photo unless user.profile_photo.nil?
-    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    gravatar_id = Digest::MD5.hexdigest(user.email).downcase
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
