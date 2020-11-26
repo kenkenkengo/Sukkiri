4625d49ecd6fb96ed5c44fb9d285475cd476f975
山本憲吾 <yamamotokengo@yamamotokengonoMacBook-Pro.local>
2020-11-26 21:30:09 +0900

    Userモデル画像修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,7 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       if user.provider == "facebook"
-        user.profile_photo = auth.info.profile_photo.
-          gsub("picture", "picture?type=large")
+        user.profile_photo = auth.info.profile_photo
       end
     end
   end
