55161fa4e03ee35bdefe674ac994f534c2acc72b
山本憲吾 <yamamotokengo@yamamotokengonoMacBook-Pro.local>
2020-11-26 17:19:14 +0900

    不要なコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,8 @@ class User < ApplicationRecord
 
   validates :username, presence: true, length: { maximum: 20 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+  validates :email, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
 
   private
 
