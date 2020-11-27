a8fac819143a23856ff40601969023d000514486
山本憲吾 <yamamotokengo@yamamotokengonoMacBook-Pro.local>
2020-11-27 17:23:16 +0900

    ユーザー編集のリンクとタイトル追加に伴うテスト修正

--- spec/requests/users_signup_spec.rb
+++ spec/requests/users_signup_spec.rb
@@ -1,6 +1,28 @@
 RSpec.describe "新規登録", type: :request do
-  it "httpリクエストは成功する" do
+  let!(:user_params) { attributes_for(:user) }
+  let!(:invalid_user_params) { attributes_for(:user, username: "") }
+
+  before do
     get new_user_registration_path
+  end
+
+  it "httpリクエストは成功する" do
     expect(response).to have_http_status(:success)
+  end
+
+  context 'パラメータが妥当な場合' do
+    it 'ユーザー登録が成功すること' do
+      expect do
+        post user_registration_path, params: { user: user_params }
+      end.to change(User, :count).by(1)
+    end
+  end
+
+  context 'パラメータが不正な場合' do
+    it 'ユーザー登録が失敗すること' do
+      expect do
+        post user_registration_path, params: { user: invalid_user_params }
+      end.not_to change(User, :count)
+    end
   end
 end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -7,28 +7,28 @@ RSpec.describe "Users", type: :system do
       visit new_user_registration_path
     end
 
-      it "「新規登録」の文字列が存在する" do
-        expect(page).to have_content '新規登録'
-      end
+    it "「新規登録」の文字列が存在する" do
+      expect(page).to have_content '新規登録'
+    end
 
-      it "有効なユーザーの登録成功フラッシュ表示" do
-        fill_in "ユーザーネーム", with: "Example User"
-        fill_in "メールアドレス", with: "user@example.com"
-        fill_in "パスワード", with: "password"
-        fill_in "パスワードの確認", with: "password"
-        click_button "登録する"
-        expect(page).to have_content "アカウント登録が完了しました"
-      end
+    it "有効なユーザーの登録成功フラッシュ表示" do
+      fill_in "ユーザーネーム", with: "Example User"
+      fill_in "メールアドレス", with: "user@example.com"
+      fill_in "パスワード", with: "password"
+      fill_in "パスワードの確認", with: "password"
+      click_button "登録する"
+      expect(page).to have_content "アカウント登録が完了しました"
+    end
 
-      it "無効なユーザーの登録失敗フラッシュ表示" do
-        fill_in "ユーザーネーム", with: ""
-        fill_in "メールアドレス", with: "user@example.com"
-        fill_in "パスワード", with: "password"
-        fill_in "パスワードの確認", with: "foobar"
-        click_button "登録する"
-        expect(page).to have_content "ユーザーネームを入力してください"
-        expect(page).to have_content "確認用パスワードとパスワードの入力が一致しません"
-      end
+    it "無効なユーザーの登録失敗フラッシュ表示" do
+      fill_in "ユーザーネーム", with: ""
+      fill_in "メールアドレス", with: "user@example.com"
+      fill_in "パスワード", with: "password"
+      fill_in "パスワードの確認", with: "foobar"
+      click_button "登録する"
+      expect(page).to have_content "ユーザーネームを入力してください"
+      expect(page).to have_content "確認用パスワードとパスワードの入力が一致しません"
+    end
   end
 
   describe "プロフィールページ" do
@@ -68,6 +68,10 @@ RSpec.describe "Users", type: :system do
       visit edit_user_registration_path(user)
     end
 
+    it "「プロフィール編集」の文字列が存在することを確認" do
+      expect(page).to have_content 'プロフィール編集'
+    end
+
     it "「パスワード変更」「アカウント削除」のリンクが存在する" do
       expect(page).to have_link 'パスワード変更', href: edit_password_path(user)
       expect(page).to have_link 'アカウント削除', href: user_registration_path
@@ -98,6 +102,10 @@ RSpec.describe "Users", type: :system do
       visit edit_password_path(user)
     end
 
+    it "「パスワード編集」の文字列が存在することを確認" do
+      expect(page).to have_content 'パスワード編集'
+    end
+
     it "有効なパスワード更新を行うと、更新成功のフラッシュが表示される" do
       fill_in "現在のパスワード", with: "foobar"
       fill_in "パスワード", with: "foobar2"
@@ -120,6 +128,10 @@ RSpec.describe "Users", type: :system do
       fill_in "パスワードの確認", with: "foobar3"
       click_button "変更する"
       expect(page).to have_content 'パスワードを正しく入力してください'
+    end
+
+    it "「プロフィール編集へ戻る」のリンクが存在する" do
+      expect(page).to have_link 'プロフィール編集へ戻る', href: edit_user_registration_path
     end
   end
 end
