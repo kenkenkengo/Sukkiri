055475ccd4307b6c7fa3b42ca312fc511c0219c5
山本憲吾 <yamamotokengo@yamamotokengonoMacBook-Pro.local>
2020-11-29 15:37:23 +0900

    groupモデルスペック追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,8 @@
 class Group < ApplicationRecord
   has_many :belongings
   has_many :users, through: :belongings, dependent: :destroy
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :admin_user_id, presence: true
 
   def admin_user
     User.find_by(id: admin_user_id)
