a3b3e3363212bbe5607eae6125d4718323ab3ea9
山本憲吾 <yamamotokengo@yamamotokengonoMacBook-Pro.local>
2020-11-30 20:04:37 +0900

    画像ファイル設定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -23,14 +23,14 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
-  process resize_to_fit: [800, 800]
+  #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  #   process resize_to_fit: [50, 50]
+  process resize_to_fill: [400, 400, "Center"]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
