ca939c0a5971834bc27edb5bdfe0a8829f90f985
山本憲吾 <yamamotokengo@yamamotokengonoMacBook-Pro.local>
2020-12-02 01:56:52 +0900

    テスト修正

--- spec/requests/groups_edit_request_spec.rb
+++ spec/requests/groups_edit_request_spec.rb
@@ -1,24 +1,28 @@
 RSpec.describe "グループ編集", type: :request do
-  let(:user) { create(:user) }
-  let(:group) { create(:group, admin_user_id: user.id) }
+  let(:user) { create(:user, :user_with_groups) }
 
   context "認可されたユーザーの場合" do
     it "レスポンスが正常に表示されること" do
       login_as(user)
+      group = user.groups.first
       get edit_group_path(group)
       expect(response).to have_http_status "200"
     end
   end
 
   context "ログインしていないユーザーの場合" do
+    before do
+      @group = user.groups.first
+    end
+
     it "編集ページへ遷移せずサインインページへリダイレクトする" do
-      get edit_group_path(group)
+      get edit_group_path(@group)
       expect(response).to have_http_status "302"
       expect(response).to redirect_to new_user_session_path
     end
 
     it "更新は失敗しサインインページへリダイレクトする" do
-      patch group_path(group), params: { group: group.name }
+      patch group_path(@group), params: { group: { name: @group.name } }
       expect(response).to have_http_status "302"
       expect(response).to redirect_to new_user_session_path
     end
--- spec/requests/groups_request_spec.rb
+++ spec/requests/groups_request_spec.rb
@@ -1,35 +1,43 @@
 RSpec.describe "グループ新規登録", type: :request do
-  let(:user) { create(:user) }
-  let(:group) { create(:group, admin_user_id: user.id) }
-  let!(:other_group) { create(:group, admin_user_id: user.id) }
+  let(:user) { create(:user, :user_with_groups) }
+  let!(:group_params) { attributes_for(:group, name: "example") }
+  let!(:invalid_group_params) { attributes_for(:group, name: "") }
 
-  before do
-    login_as(user)
-    get new_group_path
-  end
+  describe "認可されたユーザーの場合" do
+    before do
+      login_as(user)
+      get new_group_path
+    end
 
-  it "httpリクエストは成功する" do
-    expect(response).to have_http_status "200"
-  end
+    it "httpリクエストは成功する" do
+      expect(response).to have_http_status "200"
+    end
 
-  context 'パラメータが妥当な場合' do
-    it 'グループ登録が成功すること' do
-      expect do
-        post groups_path, params: { group: group.name }
-      end.to change(Group, :count).by(1)
+    context 'パラメータが不正な場合' do
+      it 'グループ名が空であればグループ登録が失敗すること' do
+        expect do
+          post groups_path, params: { group: invalid_group_params }
+        end.not_to change(Group, :count)
+      end
+
+      it 'グループ名が既に存在すればグループ登録が失敗すること' do
+        group = user.groups.first
+        expect do
+          post groups_path, params: { group: { name: group.name } }
+        end.not_to change(Group, :count)
+      end
     end
   end
 
-  context 'パラメータが不正な場合' do
-    it 'グループ名が空であればグループ登録が失敗すること' do
-      expect do
-        post groups_path, params: { group: "" }
-      end.not_to change(Group, :count)
+  describe "ログインしていないユーザーの場合" do
+    it "httpリクエストは失敗する" do
+      get new_group_path
+      expect(response).to have_http_status "302"
     end
 
-    it 'グループ名が既に存在すればグループ登録が失敗すること' do
+    it 'グループ登録が失敗すること' do
       expect do
-        post groups_path, params: { group: { name: other_group.name } }
+        post groups_path, params: { group: group_params }
       end.not_to change(Group, :count)
     end
   end
