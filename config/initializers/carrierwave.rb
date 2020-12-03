87de91afed60528e3f641b90cfe10bfd88e16b9d
山本憲吾 <yamamotokengo@yamamotokengonoMacBook-Pro.local>
2020-12-03 18:48:36 +0900

    S3設定編集

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ require 'carrierwave/storage/fog'
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory     =  ENV['AWS_S3_BUCKET']
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/sukkiribucket2'
+    config.asset_host = 'https://sukkiribucket2.s3.amazonaws.com'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
