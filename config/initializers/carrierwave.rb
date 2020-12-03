5f7495c6c735eeabaedb58ada88d17ae0b06636f
山本憲吾 <yamamotokengo@yamamotokengonoMacBook-Pro.local>
2020-12-03 23:13:01 +0900

    S3設定修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,8 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-  CarrierWave.configure do |config|
+CarrierWave.configure do |config|
+  if Rails.env.production?
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory     =  ENV['AWS_S3_BUCKET']
@@ -14,4 +15,8 @@ require 'carrierwave/storage/fog'
       region: ENV['AWS_DEFAULT_REGION'],
       path_style: true
     }
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
   end
+end
