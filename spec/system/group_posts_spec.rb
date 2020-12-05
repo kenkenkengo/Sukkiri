77bf57ccbc5a17b8473843079bcf28fdd9bcd19e
山本憲吾 <yamamotokengo@yamamotokengonoMacBook-Pro.local>
2020-12-06 03:00:43 +0900

    投稿一覧ページlike機能のシステムスペック追加

--- spec/system/group_posts_spec.rb
+++ spec/system/group_posts_spec.rb
@@ -1,8 +1,8 @@
 RSpec.describe "投稿一覧", type: :system do
   let(:user) { create(:user, :user_with_groups_and_posts) }
-  let(:other_user) { create(:user, :user_with_groups_and_posts) }
+  let(:other_user) { create(:user, :other_user_with_groups_and_posts) }
 
-  describe "新規投稿フォーム" do
+  context "新規投稿フォーム" do
     before do
       login_as(user)
       @group = user.groups.first
@@ -31,26 +31,53 @@ RSpec.describe "投稿一覧", type: :system do
       click_button "登録する"
       expect(page).to have_content "写真の選択をしてください"
     end
+  end
+
+  context "投稿一覧" do
+    before do
+      login_as(user)
+      @group = user.groups.first
+      @post = user.posts.first
+      visit group_posts_path(@group)
+    end
 
     it "投稿者名、写真名、画像が存在する" do
-      post = user.posts.first
-      expect(page).to have_content user.username
-      expect(page).to have_content post.content
+      expect(page).to have_content @post.user.username
+      expect(page).to have_content @post.content
       expect(page).to have_selector("img[src$='test_image.jpg']")
     end
 
-    it "自分の投稿であれば「投稿編集」リンクが存在する" do
-      post = user.posts.first
-      expect(page).to have_link '投稿編集', href: edit_group_post_path(@group, post)
+    it "異なるグループの投稿は表示されない" do
+      other_post = other_user.posts.first
+      expect(page).not_to have_content other_user.username
+      expect(page).not_to have_content other_post.content
+      expect(page).not_to have_selector("img[src$='test_image2.jpg']")
     end
 
-    it "ログインユーザー以外の投稿であれば「投稿編集」リンクが存在しない" do
-      post = other_user.posts.first
-      expect(page).not_to have_link '投稿編集', href: edit_group_post_path(@group, post)
+    it "自分の投稿であれば「投稿編集」リンクが存在する" do
+      expect(page).to have_link '投稿編集', href: edit_group_post_path(@group, @post)
+    end
+
+    it "自分以外の投稿であれば「投稿編集」リンクが存在しない" do
+      other_post = other_user.posts.first
+      expect(page).not_to have_link '投稿編集', href: edit_group_post_path(@group, other_post)
+    end
+
+    it "keep登録/解除ができること", js: true do
+      link = find('.like')
+      expect(link[:href]).to include group_post_likes_path(@group, @post)
+      link.click
+      link = find('.unlike')
+      expect(link[:href]).to include
+      group_post_like_path(@group, @post, Like.find_by(user_id: user.id, post_id: @post.id))
+
+      link.click
+      link = find('.like')
+      expect(link[:href]).to include group_post_likes_path(@group, @post)
     end
   end
 
-  describe "投稿編集ページ" do
+  context "投稿編集ページ" do
     before do
       login_as(user)
       @group = user.groups.first
