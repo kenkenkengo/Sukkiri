d77d39f31cc28a65d348a0beffdcd6a31a3aacc3
山本憲吾 <yamamotokengo@yamamotokengonoMacBook-Pro.local>
2020-12-05 13:21:05 +0900

    _post.html.erb中のlike_iconとcomment_formのパーシャル化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @likes = current_user.likes
+    @likes = current_user.likes.includes(:user).order(id: "DESC")
   end
 
   def create
@@ -17,6 +17,7 @@ class LikesController < ApplicationController
   def destroy
     @like = Like.find_by(id: params[:id])
     @post = @like.post
+    @group = @post.group
     if @like.destroy
       respond_to :js
     end
