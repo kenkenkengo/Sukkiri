5858d3bb16a3d387f0d590c688c339891ac40118
山本憲吾 <yamamotokengo@yamamotokengonoMacBook-Pro.local>
2020-12-15 19:09:34 +0900

    carrierwaveファイル修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,18 +2,16 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.storage :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_directory     =  ENV['AWS_S3_BUCKET']
-    config.asset_host = 'https://sukkiribucket2.s3.amazonaws.com'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_DEFAULT_REGION'],
-      path_style: true
-    }
-  end
+CarrierWave.configure do |config|
+  config.storage :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_directory     =  ENV['AWS_S3_BUCKET']
+  config.asset_host = 'https://sukkiribucket2.s3.amazonaws.com'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: ENV['AWS_DEFAULT_REGION'],
+    path_style: true
+  }
 end
